6c16c9870a02a86a7fba871d2d1664ee8175e703
nkosi-tauro <nkosilati23@yahoo.com>
2021-04-30 00:12:46 +0200

    added Navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation'
 
 function App() {
   return (
     <div className="App">
+      <Navigation/>
       <h1>Main Page</h1>
     </div>
   );
