68dbfab473cc2e19fd92f0bf935c4f41f28545a6
nkosi-tauro <nkosilati23@yahoo.com>
2021-05-01 13:35:10 +0200

    Added Heading + styles to landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import BackImage from '../assets/backImg.jpg'
+import './landingStyles.css'
 
 function Landing() {
     return (
-        <div>
-            <div className="landing Page" style={{"background-image": `url(${BackImage})`,"height": "500px", "background-size": "cover", "background-repeat": "no-repeat"}}>
-                <h1>Land here</h1>
+        <div className="landing-Page">
+            <div style={{"background-image": `url(${BackImage})`,"height": "500px", "background-size": "cover", "background-repeat": "no-repeat"}}>
+                <h2 className="landh2"><span>Dribblytics:<span class='spacer'></span><br /><span class='spacer'></span>Basketball Analytics Made Simple</span></h2>
             </div>
             <div>
                 <footer>Test</footer>
