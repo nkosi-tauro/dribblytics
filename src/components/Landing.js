6929209ff5fcaa98196a37d5ff33ed14e745f4ea
nkosi-tauro <nkosilati23@yahoo.com>
2021-05-01 13:48:00 +0200

    added animated h2

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import BackImage from '../assets/backImg.jpg'
 import './landingStyles.css'
+import { useSpring, animated } from 'react-spring'
 
 function Landing() {
+    const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 }, delay: 500 })
     return (
         <div className="landing-Page">
             <div style={{"background-image": `url(${BackImage})`,"height": "500px", "background-size": "cover", "background-repeat": "no-repeat"}}>
-                <h2 className="landh2"><span>Dribblytics:<span class='spacer'></span><br /><span class='spacer'></span>Basketball Analytics Made Simple</span></h2>
+                <animated.h2 className="landh2" style={props}><span>Dribblytics:<span class='spacer'></span><br /><span class='spacer'></span>Basketball Analytics Made Simple</span></animated.h2>
             </div>
             <div>
                 <footer>Test</footer>
