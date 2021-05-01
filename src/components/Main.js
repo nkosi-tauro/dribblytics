81a508f20d2e1fd830bfb4829baf77918e709f37
nkosi-tauro <nkosilati23@yahoo.com>
2021-05-01 14:44:58 +0200

    Added displayName + new button style

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useContext} from "react";
 import firebaseConfig from "../firebase";
+import { AuthContext } from "./Auth";
 
 const Main = () => {
+  const { currentUser } = useContext(AuthContext);
+  const loggedInUser = currentUser.displayName
   return (
-    <>
-      <h1>Welcome  </h1>
-      <button onClick={() => firebaseConfig.auth().signOut()}>Sign out</button>
-    </>
+    <div class="px-2 text-center">
+      <div>
+        <h1 class="text-2xl p-2">Welcome {loggedInUser} </h1>
+      </div>
+      <div>
+        <button class="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-red-500 rounded shadow ripple hover:shadow-lg hover:bg-red-600 focus:outline-none" onClick={() => firebaseConfig.auth().signOut()}>Sign out</button>
+      </div>
+    </div>
   );
 };
 
