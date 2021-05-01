9300045a0f557b342d29f4d13ab1030dcb5090ac
nkosi-tauro <nkosilati23@yahoo.com>
2021-05-01 15:12:08 +0200

    update css properties to React

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,7 +7,7 @@ function Landing() {
     const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 }, delay: 500 })
     return (
         <div className="landing-Page">
-            <div style={{"background-image": `url(${BackImage})`,"height": "500px", "background-size": "cover", "background-repeat": "no-repeat"}}>
+            <div style={{"backgroundImage": `url(${BackImage})`,"height": "500px", "backgroundSize": "cover", "backgroundRepeat": "no-repeat"}}>
                 <animated.h2 className="landh2" style={props}><span>Dribblytics:<span class='spacer'></span><br /><span class='spacer'></span>Basketball Analytics Made Simple</span></animated.h2>
             </div>
             <div>
